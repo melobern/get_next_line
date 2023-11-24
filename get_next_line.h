14c3f3d58c9adfe1d0724fa8bc6e4adca37c0ade
Melodie Bernard <melodie.bernard123@yahoo.fr>
2023-11-24 13:58:49 +0100

    to see later

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:41:27 by mbernard          #+#    #+#             */
-/*   Updated: 2023/11/21 13:43:23 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:15:08 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
 
 #endif
