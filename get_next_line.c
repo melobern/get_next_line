4b35ae4cc3648050ccaf4ffbdb8b72b9787987d0
Melodie Bernard <mbernard@z1r10p3.42lyon.fr>
2023-11-24 17:56:44 +0100

    stand by, doing it later

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:45:35 by mbernard          #+#    #+#             */
-/*   Updated: 2023/11/22 09:14:49 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/11/24 17:56:21 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static int	ft_contains_end_line(char *str)
+{
+	int	x;
+
+	x = 0;
+	while (str
+}
+
 char *get_next_line(int fd)
 {
 	static char	*stash;
+	char	*buf[5];
 	char	*tmp;
 	char	*line;
 
-	tmp = read(fd, )	
-	while(){
+	tmp = read(fd, buf, 5);	
+	while(tmp){
 	}
 }
