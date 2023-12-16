e723b3e4309fc4ce04e3aeb9a3dcf0ec59e7f227
Mélodie <melodie.bernard123@yahoo.fr>
2023-12-16 09:55:45 +0100

    grr

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,24 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:41:27 by mbernard          #+#    #+#             */
-/*   Updated: 2023/12/15 18:09:25 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/12/16 09:52:02 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h> // TO REMOVE
+# include <stdio.h> // TO REMOVE
 # include <stdlib.h>
 # include <unistd.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
 char	*get_next_line(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 char	*ft_strnjoin(char *s1, char *s2, size_t size);
-size_t	ft_strlen(char *s);
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
 
 // TO DELETE //
