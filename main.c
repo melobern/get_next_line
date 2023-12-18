6c38c4ebfa937f69511c4809b453a9680f6c1530
mbernard <mbernard@student.42lyon.fr>
2023-12-18 16:14:05 +0100

    nearly ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:22:35 by mbernard          #+#    #+#             */
-/*   Updated: 2023/12/17 15:22:23 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/12/18 15:56:26 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,21 @@ int	main(int ac, char **av)
 	int	fd;
 	char	*line;
 
-setbuf(stdout, NULL);
+//setbuf(stdout, NULL);
 	if (ac > 1)
 	{
 		int x = 1;
 		fd = open(av[1], O_RDONLY);
-		while (x < 42) {
+		while (x) {
 			//printf("ITERATION NUMBER %i\n", x + 1);
 			line = get_next_line(fd);
 			if (!line)
 				break;
-			printf("LINE : %s\n", line);
+		//	printf("\033[0;36m");
+		printf("LINE %s", line);
+		//	printf("\033[0m"); // Remet la couleur par défaut
 			x++;
+
 			free(line);
 		}
 		close(fd);
