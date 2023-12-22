f99718684a6fbfcf2d175e275f673899e91f20fa
mbernard <mbernard@student.42lyon.fr>
2023-12-22 14:11:28 +0100

    bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <mbernard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 13:45:35 by mbernard          #+#    #+#             */
+/*   Updated: 2023/12/22 14:06:13 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int	ft_contains_end_line(char *str)
+{
+	int	x;
+
+	x = 0;
+	while (str && str[x])
+	{
+		if (str[x] == '\n')
+			return (x);
+		x++;
+	}
+	return (-1);
+}
+
+void	ft_clear_stash(char (*stash)[BUFFER_SIZE + 1], int fd)
+{
+	if (fd < 0)
+	{
+		fd = 0;
+		while (fd < 1024)
+		{
+			stash[fd][0] = '\0';
+			fd++;
+		}
+	}
+	else
+		stash[fd][0] = '\0';
+}
+
+static char	*ft_read(int fd, char *stash, char *line)
+{
+	long int	bytes_read;
+
+	if (stash[0])
+		line = ft_strnjoin(line, stash, ft_strlen(stash));
+	if (line == NULL)
+		return (NULL);
+	if (stash[0] && ft_contains_end_line(stash) != -1)
+		return (line);
+	bytes_read = read(fd, stash, BUFFER_SIZE);
+	if (bytes_read <= 0 && !line[0])
+		return (free(line), NULL);
+	while (bytes_read > 0)
+	{
+		stash[bytes_read] = '\0';
+		if (ft_contains_end_line(stash) != -1)
+			break ;
+		line = ft_strnjoin(line, stash, bytes_read);
+		if (line == NULL)
+			return (NULL);
+		bytes_read = read(fd, stash, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(line), NULL);
+	}
+	return (line);
+}
+
+static char	*ft_line(char *stash, char *line)
+{
+	int		size;
+	int		len;
+	char	*new;
+
+	if (!line)
+		return (NULL);
+	len = ft_contains_end_line(line);
+	if (len != -1)
+	{
+		new = ft_calloc(1, 1);
+		new = ft_strnjoin(new, line, len + 1);
+		return (free(line), new);
+	}
+	else
+	{
+		size = ft_contains_end_line(stash) + 1;
+		line = ft_strnjoin(line, stash, size);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	stash[1024][BUFFER_SIZE + 1];
+	char		*line;
+	int			rest;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, stash, 0) < 0)
+	{
+		ft_clear_stash(stash, fd);
+		return (NULL);
+	}
+	line = ft_calloc(1, 1);
+	line = ft_read(fd, stash[fd], line);
+	line = ft_line(stash[fd], line);
+	if (line == NULL)
+	{
+		ft_clear_stash(stash, fd);
+		return (NULL);
+	}
+	rest = ft_contains_end_line(stash[fd]) + 1;
+	if (rest > 0 && stash[fd][rest - 1])
+		ft_strncpy(stash[fd], stash[fd] + rest, BUFFER_SIZE);
+	else
+		ft_clear_stash(stash, fd);
+	return (line);
+}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:22:35 by mbernard          #+#    #+#             */
-/*   Updated: 2023/12/21 20:04:32 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/12/22 13:58:56 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,61 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	main(int ac, char **av)
+void	one_fd_test(int ac, char **av)
 {
-	int		fd;
 	char	*line;
 	int		x;
+	int		fd;
 
-	if (ac > 1)
+	fd = open(av[1], O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
 	{
-		x = 1;
-		fd = open(av[1], O_RDONLY);
-		while (x)
-		{
-			line = get_next_line(fd);
-			if (!line)
-				break ;
-			printf("\033[0;32m");
-			printf("LINE %s", line);
-			printf("\033[0m");
-			free(line);
-		}
-		close(fd);
+		printf("\033[0;32m");
+		printf("LINE %s", line);
+		printf("\033[0m");
+		free(line);
+		line = get_next_line(fd);
+		if (!line)
+			break ;
 	}
+	close(fd);
+}
+
+void	two_fd_test(int ac, char **av)
+{
+	int		fd;
+	int		fd_two;
+	char	*line;
+
+	fd = open(av[1], O_RDONLY);
+	fd_two = open(av[2], O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		printf("\033[0;32m");
+		printf("LINE %s", line);
+		printf("\033[0m");
+		free(line);
+		line = get_next_line(fd_two);
+		if (!line)
+			break ;
+		printf("\033[0;36m");
+		printf("LINE %s", line);
+		printf("\033[0m");
+		free(line);
+	}
+	close(fd);
+	close(fd_two);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		one_fd_test(ac, av);
+	else if (ac > 2)
+		two_fd_test(ac, av);
 	return (0);
 }
